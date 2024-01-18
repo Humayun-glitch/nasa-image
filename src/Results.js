a58119264db57a5c31bceca59b81038676e8545b
Humayun <humayunsaeed75@gmail.com>
2024-01-18 21:14:11 +0200

    .

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,4 @@
-// Results.js
-
 import React from 'react';
-import './SearchForm.js';
 
 const Results = ({ results }) => {
   return (
@@ -9,28 +6,31 @@ const Results = ({ results }) => {
       {results.map((result) => (
         <div key={result.data[0].nasa_id} className="result-item">
           {result.data[0].media_type === 'image' ? (
-            <img
-              src={result.links[0].href}
-              alt={result.data[0].title}
-              className="result-image"
-            />
+            <a
+              href={result.links[0].href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={result.links[0].href}
+                alt={result.data[0].title}
+                className="result-image"
+              />
+            </a>
           ) : (
-            <div>
-              <video
-                controls
-                width="100%"
-                height="100%"
-                className="result-video"
-                poster={result.links[0].href}
-              >
-                {/* Provide multiple source formats for better browser compatibility */}
-                <source src={result.links[0].href} type="video/mp4" />
-                <source src={result.links[0].href} type="video/webm" />
-                Your browser does not support the video tag.
-              </video>
-              <p>{result.data[0].title}</p>
-            </div>
+            <video
+              controls
+              width="100%"
+              height="100%"
+              className="result-video"
+              poster={result.links[0].href}
+              onClick={() => window.open(result.links[0].href, '_blank')}
+            >
+              <source src={result.links[0].href} type={result.links[0].href.split('.').pop() === 'webm' ? "webm" : "mp4"} />
+              Your browser does not support the video tag.
+            </video>
           )}
+          <p>{result.data[0].title}</p>
         </div>
       ))}
     </div>
