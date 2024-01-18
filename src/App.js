a9836ca820de6b5afba1394c83b783f754d96f42
Humayun <humayunsaeed75@gmail.com>
2024-01-18 08:45:04 +0200

    updated.

--- src/App.js
+++ src/App.js
@@ -7,32 +7,14 @@ import './App.css';
 
 const App = () => {
   const [results, setResults] = useState([]);
-  const [mediaType, setMediaType] = useState('image');
+  const [mediaType] = useState('image');
 
-  const handleToggle = (type) => {
-    setMediaType(type);
-  };
+
 
   return (
     <div className="container">
       <h1>NASA Image and Video Library</h1>
       <div className="toggle-container">
-        <label>
-          Images
-          <input
-            type="checkbox"
-            checked={mediaType === 'image'}
-            onChange={() => handleToggle('image')}
-          />
-        </label>
-        <label>
-          Videos
-          <input
-            type="checkbox"
-            checked={mediaType === 'video'}
-            onChange={() => handleToggle('video')}
-          />
-        </label>
       </div>
       <SearchForm setResults={setResults} mediaType={mediaType} />
       <Results results={results} />
